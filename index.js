0485ffb23be2dbf025849c1e1bcfeadaf3de7747
face-hh <xxfacexc@gmail.com>
2021-08-25 14:42:30 +0000

    some bug fixes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-/* eslint-disable */
+/* eslint-disable no-inner-declarations*/
+
 const Discord = require('discord.js');
 require('dotenv').config();
 const voiceDiscord = require('@discordjs/voice');
@@ -75,7 +76,7 @@ client.on('messageCreate', async message => {
 				type: 1,
 				components: [
 					{
-						type: 2, 
+						type: 2,
 						style: 'SECONDARY',
 						custom_id: 'left',
 						label: 'Left',
@@ -84,24 +85,23 @@ client.on('messageCreate', async message => {
 						type: 2,
 						style: 'PRIMARY',
 						custom_id: 'middle',
-						label: 'Middle'
+						label: 'Middle',
 					},
 					{
 						type: 2,
 						style: 'SECONDARY',
 						custom_id: 'right',
 						label: 'Right',
-					}
-				]
-			}
+					},
+				],
+			},
 		];
 
 		const msg = await message.channel.send({
 			content: randomPos,
 			components: componentsArray,
 		});
-
-		function update(){
+		function update() {
 			randomized = Math.floor(Math.random() * Object.keys(positions).length);
 			randomPos = positions[Object.keys(positions)[randomized]];
 
@@ -111,7 +111,7 @@ client.on('messageCreate', async message => {
 			});
 		}
 		setInterval(() => {
-			if(gameEnded == false) return update()
+			if(gameEnded == false) return update();
 		}, 1000);
 
 		const filter = button => {
@@ -119,14 +119,108 @@ client.on('messageCreate', async message => {
 		};
 		const button = await msg.awaitMessageComponent({ filter: filter, componentType: 'BUTTON', max: 1 });
 
-		if(button.customId !== Object.keys(positions)[randomized]){
+		if(button.customId !== Object.keys(positions)[randomized]) {
 			gameEnded = true;
 			return button.reply({ content: 'You won!' });
-		} else {
+		}
+		else {
 			gameEnded = true;
 			return button.reply({ content: 'You lose...' });
-		};
+		}
 	}
-})
+	if(isCommand('fightgun')) {
+		const opponent = message.mentions.users.first();
+		const positions = {
+			three: '_ _        :levitate: :point_right:      **3**        :point_left: :levitate:',
+			two: '_ _        :levitate: :point_right:      **2**        :point_left: :levitate:',
+			one: '_ _        :levitate: :point_right:      **1**        :point_left: :levitate:',
+			go: '_ _        :levitate: :point_right:      **GO!**        :point_left: :levitate:',
+			ended1: '_ _     :levitate: :point_right:      **STOP!**        :skull_crossbones: :levitate:',
+			ended2: '_ _     :levitate: :skull_crossbones:      **STOP!**        :point_left: :levitate:',
+		};
+
+		const componentsArray = [
+			{
+				type: 1,
+				components: [
+					{
+						type: 2,
+						label: 'Shoot!',
+						custom_id: 'shoot1',
+						style: 'PRIMARY',
+						disabled: true,
+					},
+					{
+						type: 2,
+						label: '\u200b',
+						custom_id: 'id lol useless',
+						style: 'SECONDARY',
+						disabled: true,
+					},
+					{
+						type: 2,
+						label: 'Shoot!',
+						custom_id: 'shoot2',
+						style: 'DANGER',
+						disabled: true,
+					},
+				],
+			},
+		];
+
+		const msg = await message.channel.send({
+			content: positions.three,
+			components: componentsArray,
+		});
+
+		function countdown() {
+			setTimeout(() => {
+				msg.edit({
+					content: positions.two,
+					components: componentsArray,
+				});
+			}, 1000);
+			setTimeout(() => {
+				msg.edit({
+					content: positions.one,
+					components: componentsArray,
+				});
+			}, 2000);
+			setTimeout(() => {
+				componentsArray[0].components[0].disabled = false;
+				componentsArray[0].components[2].disabled = false;
+				msg.edit({
+					content: positions.go,
+					components: componentsArray,
+				});
+			}, 3000);
+		}
+		countdown();
+
+		const filter = button => {
+			return button.user.id == message.author.id || button.user.id == opponent.id;
+		};
+
+		const button = await msg.awaitMessageComponent({ filter: filter, componentType: 'BUTTON', max: 1 });
+
+		componentsArray[0].components[0].disabled = true;
+		componentsArray[0].components[2].disabled = true;
+
+		if(button.customId === 'shoot1' && button.user.id == message.author.id) {
+			msg.edit({
+				content: positions.ended1,
+				components: componentsArray,
+			});
+			return button.reply({ content: `<@${message.author.id}> won!` });
+		}
+		else if(button.customId === 'shoot2' && button.user.id == opponent.id) {
+			msg.edit({
+				content: positions.ended1,
+				components: componentsArray,
+			});
+			return button.reply({ content: `<@${opponent.id}> won!` });
+		}
+	}
+});
 
 client.login(process.env.token);
